5647bdc0ed49dc5508985c592918042e2384c227
martinmunilla <martinmunillas@gmail.com>
2021-03-21 17:38:24 -0300

    initial commit

---
+++ src/components/Atoms/Button/Button.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { colors, defaultColor } from '../../../defaults';
+import { QuaantumUIProps } from '../../../types';
+
+export interface ButtonProps extends QuaantumUIProps {
+  variant?: 'solid' | 'outline';
+}
+
+const solidVariant = (props: ButtonProps) => `
+  color: ${colors['white']};
+  background: ${colors[props.color || defaultColor]};
+`;
+
+const outlineVariant = (props: ButtonProps) => `
+  color: ${colors[props.color || defaultColor]};
+  border: 2px solid ${colors[props.color || defaultColor]};
+  background-color: transparent;
+`;
+
+const Button = styled.button<ButtonProps>`
+  border: none;
+  border-radius: ${({ round }) => (typeof round === 'number' ? `${round}px` : round) || '50px'};
+  padding: 10px 20px;
+  font-weight: 600;
+  min-width: 100px;
+  max-width: 300px;
+  font-size: 18px;
+  cursor: pointer;
+  outline: none;
+  ${(props) => (props.variant === 'solid' ? solidVariant(props) : outlineVariant(props))}
+
+  &:focus ${({ _focus }) =>
+    _focus
+      ? _focus
+      : `{
+    box-shadow: 1px 1px 1px 1px blue;
+  }`}
+`;
+
+export default Button;
