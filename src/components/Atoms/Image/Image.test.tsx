c4521d1c174690d38a9a489eb2a321eef99e35de
martinmunilla <martinmunillas@gmail.com>
2021-04-02 22:23:44 -0300

    hotfix/fix-image-test

--- src/components/Atoms/Image/Image.test.tsx
+++ src/components/Atoms/Image/Image.test.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import Image from './Image';
 import { render, screen } from '@testing-library/react';
 
-
 const setup = (props) => render(<Image {...props} />);
 
 describe('Image', () => {
   it('renders', () => {
-    setup({children: 'foo'});
-    expect(screen.getByText('foo'));
+    setup({ 'data-testid': 'foo' });
+    expect(screen.getByTestId('foo'));
   });
 });
