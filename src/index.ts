8d63fafd319d959807a3bb25120019e4a3fb8d4d
Martin Munilla <65319051+martinmunillas@users.noreply.github.com>
2021-04-04 13:20:53 -0400

    Hotfix/fix version (#17)
    * 0.2.5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './components';
+export { extendTheme } from './utils/funcs/extendTheme/extendTheme';
+export { theme } from './defaults/theme';
+export { useTheme } from './utils/hooks/useTheme';
