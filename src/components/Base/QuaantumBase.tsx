b02785d0ea3af9e1fc8d9d01142c7d614a181a94
martinmunillas <martinmunillas@gmail.com>
2021-06-12 10:04:13 -0400

    chore: solved build errors

--- src/components/Base/QuaantumBase.tsx
+++ src/components/Base/QuaantumBase.tsx
@@ -6,33 +6,28 @@ import { HTML } from './HTML';
 
 interface QuaantumBaseProps extends QuaantumProps {
   exclude?: readonly string[] | string[];
+  ref?: React.Ref<HTMLElement>;
 }
 
-export const QuaantumBase = React.forwardRef(
-  <T extends true | 'ok' = true>(
-    {
-      as: As,
-      exclude,
-      ...props
-    }: Record<string, any> & (T extends true ? QuaantumBaseProps : Record<string, any>),
-    ref: React.Ref<HTMLElement>
-  ) => {
-    const finalProps = useQuaantum(props, exclude);
+export const QuaantumBase = <T extends true | 'ok' = true>({
+  as: As,
+  exclude,
+  ref,
+  ...props
+}: Record<string, any> & (T extends true ? QuaantumBaseProps : Record<string, any>)) => {
+  const finalProps = useQuaantum(props, exclude);
 
-    let internalAs: string = useMemo(() => As || '', [As]);
+  let internalAs: string = useMemo(() => As || '', [As]);
 
-    if (!internalAs) {
-      internalAs = 'div';
-    }
-
-    if (!(internalAs in HTML)) {
-      throw new Error(
-        `${internalAs} is not a valid dom element, please replace it with a valid one`
-      );
-    }
-
-    const Selected = useMemo(() => HTML[internalAs as DomElement], [internalAs]);
-
-    return <Selected {...finalProps} ref={ref} />;
+  if (!internalAs) {
+    internalAs = 'div';
   }
-);
+
+  if (!(internalAs in HTML)) {
+    throw new Error(`${internalAs} is not a valid dom element, please replace it with a valid one`);
+  }
+
+  const Selected = useMemo(() => HTML[internalAs as DomElement], [internalAs]);
+
+  return <Selected {...finalProps} ref={ref} />;
+};
