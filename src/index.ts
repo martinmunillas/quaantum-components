6d3bc0f672687436b160e7c6b6cd4ed77d78a44c
Martin Munilla <65319051+martinmunillas@users.noreply.github.com>
2021-10-03 15:03:19 -0700

    feat: fixed as prop and typing bundle (#69)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,59 @@
-export * from './components';
 export { extendTheme } from './utils/funcs/extendTheme/extendTheme';
 export { theme } from './defaults/theme';
 export { useTheme } from './utils/hooks/useTheme';
 export { useOnClickOutside } from './utils/hooks/useOnClickOutside';
 export { useQuaantum } from './utils/hooks/useQuaantum';
 export { useToggleScroll } from './utils/hooks/useToggleScroll';
+
+export { default as Button, ButtonProps } from './components/Atoms/Button/Button';
+
+export { default as Heading, HeadingProps } from './components/Atoms/Heading/Heading';
+
+export { default as Flex, FlexProps } from './components/Atoms/Flex/Flex';
+
+export {
+  default as QuaantumProvider,
+  QuaantumProviderProps,
+} from './components/Providers/QuaantumProvider/QuaantumProvider';
+
+export { default as Box, BoxProps } from './components/Atoms/Box/Box';
+
+export { default as Grid, GridProps } from './components/Atoms/Grid/Grid';
+
+export { default as Text, TextProps } from './components/Atoms/Text/Text';
+
+export { default as Input, InputProps } from './components/Atoms/Input/Input';
+
+export { default as TextArea, TextAreaProps } from './components/Atoms/TextArea/TextArea';
+
+export { default as Img, ImgProps } from './components/Atoms/Img/Img';
+
+export { default as Link, LinkProps } from './components/Atoms/Link/Link';
+
+export {
+  default as UnorderedList,
+  UnorderedListProps,
+} from './components/Atoms/UnorderedList/UnorderedList';
+
+export {
+  default as OrderedList,
+  OrderedListProps,
+} from './components/Atoms/OrderedList/OrderedList';
+
+export { default as ListItem, ListItemProps } from './components/Atoms/ListItem/ListItem';
+
+export { default as Chart, ChartProps } from './components/Organisms/Charts/Chart';
+export { ChartAxisProps } from './components/Organisms/Charts/childs/Axis/ChartAxis';
+export { ChartAxesProps } from './components/Organisms/Charts/childs/Axes/ChartAxes';
+export { ChartLineProps } from './components/Organisms/Charts/childs/Line/ChartLine';
+export { ChartBarsProps } from './components/Organisms/Charts/childs/Bars/ChartBars';
+export { ChartTitleProps } from './components/Organisms/Charts/childs/Title/ChartTitle';
+
+export { default as Modal, ModalProps } from './components/Organisms/Modals/Modal';
+export { ModalCloseButtonProps } from './components/Organisms/Modals/childs/CloseButton/ModalCloseButton';
+export { ModalBodyProps } from './components/Organisms/Modals/childs/Body/ModalBody';
+export { ModalFooterProps } from './components/Organisms/Modals/childs/Footer/ModalFooter';
+export { ModalHeaderProps } from './components/Organisms/Modals/childs/Header/ModalHeader';
+export { ModalOverlayProps } from './components/Organisms/Modals/childs/Overlay/ModalOverlay';
+
+export { default as ErrorCatcher } from './components/Utils/ErrorCatcher';
