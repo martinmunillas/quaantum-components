0a1e8a3ab11f19ea5e3cf461a652b6a5568fada0
Martin Munilla <65319051+martinmunillas@users.noreply.github.com>
2021-12-08 20:23:14 -0300

    Feature/switch component (#71)
    * feat: switch component
    
    * 0.2.4

--- src/components/Atoms/Flex/Flex.stories.tsx
+++ src/components/Atoms/Flex/Flex.stories.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import type { Meta, Story } from '@storybook/react';
-import Flex from './Flex';
+import Flex, { FlexProps } from './Flex';
 import QuaantumProvider from '../../Providers/QuaantumProvider/QuaantumProvider';
 import { theme } from '../../../defaults/theme';
-import { Box, ButtonProps } from '../..';
+import Box from '../Box/Box';
 
 export default {
   title: 'Atoms/Flex',
   component: Flex,
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => (
+const Template: Story<FlexProps> = (args) => (
   <QuaantumProvider theme={theme}>
     <Flex {...args} width='100%'>
       <Box width='100px' height='100px' bgColor='blue' />
