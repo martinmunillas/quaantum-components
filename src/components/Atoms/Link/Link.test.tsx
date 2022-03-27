1432a2435a0f78c82609b27c02ecedb00749f817
martinmunillas <martinmunillas@gmail.com>
2022-03-27 11:46:47 -0300

    hotfix

--- src/components/Atoms/Link/Link.test.tsx
+++ src/components/Atoms/Link/Link.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from './Link';
 import { render, screen, cleanup } from '@testing-library/react';
-import { Link as RouterLink } from 'react-router-dom';
 
 describe('Link', () => {
   it('renders correctly', () => {
